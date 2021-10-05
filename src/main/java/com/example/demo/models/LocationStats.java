01ab6668f0201feb95210c64248037cb36f2665e
Joaquin Aman <Joaquin Aman>
2021-10-05 01:06:36 -0300

    first commit

---
+++ src/main/java/com/example/demo/models/LocationStats.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+@Data
+public class LocationStats {
+
+	private String state;
+	private String country;
+	private int latestTotalCases;
+	private int diffFromPrevDay;
+}
